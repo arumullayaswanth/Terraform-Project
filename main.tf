4f515815dcf533f0638a723b69f134b4caac17f5
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-04-01 21:15:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 
 provider "aws" {
-region = "ap-southeast-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-02f624c08a83ca16f"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "my-Key pair"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1a"
+  availability_zone = "us-east-1a"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my app created by terraform infrastructurte by raham sir server-1" > /var/www/html/index.html
+echo "hai all this is my app created by terraform infrastructurte by yaswanth server-1" > /var/www/html/index.html
 EOF
   tags = {
     Name = "web-server-1"
@@ -23,18 +23,18 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-02f624c08a83ca16f"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "my-Key pair"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1b"
+  availability_zone = "us-east-lb"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my website created by terraform infrastructurte by raham sir server-2" > /var/www/html/index.html
+echo "hai all this is my website created by terraform infrastructurte by yaswanth server-2" > /var/www/html/index.html
 EOF
   tags = {
     Name = "web-server-2"
@@ -42,22 +42,22 @@ EOF
 }
 
 resource "aws_instance" "three" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-02f624c08a83ca16f"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "my-Key pair"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "app-server-1"
   }
 }
 
 resource "aws_instance" "four" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-02f624c08a83ca16f"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "my-Key pair"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1b"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "app-server-2"
   }
@@ -87,10 +87,24 @@ resource "aws_security_group" "five" {
   }
 }
 
-resource "aws_s3_bucket" "six" {
-  bucket = "devopsbyrahamterraserverbucket9988oo9988"
+# Create an S3 Bucket with Tags
+resource "aws_s3_bucket" "example" {
+  bucket = "yaswanth523192"  # Must be globally unique
+
+  tags = {
+    Name        = "MyS3Bucket"
+    Environment = "Production"
+  }
 }
 
+# Enable Versioning on the S3 Bucket
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+# Create an IAM user with additional configurations
 resource "aws_iam_user" "seven" {
 for_each = var.user_names
 name = each.value
@@ -102,10 +116,17 @@ type = set(string)
 default = ["user1", "user2", "user3", "user4"]
 }
 
+# Creates an EBS volume with encryption enabled
 resource "aws_ebs_volume" "eight" {
- availability_zone = "ap-southeast-1a"
-  size = 40
+  size              = 20  
+  availability_zone = "us-east-1b"  
+  encrypted         = true  
+  type              = "gp3"  
+  iops              = 3000  
+  throughput        = 125  
+
   tags = {
-    Name = "ebs-001"
+    Name = "yaswanth-ebs"  
   }
 }
+
